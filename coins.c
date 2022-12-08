18e328c3c9f2a36a832b06ac93eb175e96a1219d
KorneenkovNikita <119732947+KorneenkovNikita@users.noreply.github.com>
2022-12-08 14:50:50 +0200

    Update coins.c

--- coins.c
+++ coins.c
@@ -2,25 +2,30 @@
 #include "coins.h"
 
 int Change (const int coins[], int money, int min_coins[], int coins_used[]) {
-    for (int cents = 0; cents < money + 1; ++cents) {
+    for (int cents = 0; cents < money + 1; ++cents) 
+    {
         int coin_count = cents;
         int new_coin = 1;
+        int ArrSize = 0;
 
-        int array_size = 0;
-
-        for (int j = 0; j < 5; ++j) {
+        for (int j = 0; j < 5; ++j) 
+        {
             if(coins[j] <= cents)
-                array_size++;
+                ArrSize++;
         }
 
-        int allowed_coins[array_size];
+        int allowed_coins[ArrSize];
 
-        for (int j = 0; j < 5; ++j) {
-            if(coins[j] <= cents) allowed_coins[j] = coins[j];
+        for (int j = 0; j < 5; ++j) 
+        {
+            if(coins[j] <= cents) 
+                allowed_coins[j] = coins[j];
         }
 
-        for (int j = 0; j < array_size; ++j) {
-            if(min_coins[cents - allowed_coins[j]] + 1 < coin_count){
+        for (int j = 0; j < ArrSize; ++j) 
+        {
+            if(min_coins[cents - allowed_coins[j]] + 1 < coin_count)
+            {
                 coin_count = min_coins[cents - allowed_coins[j]] + 1;
                 new_coin = allowed_coins[j];
             }
@@ -31,9 +36,11 @@ int Change (const int coins[], int money, int min_coins[], int coins_used[]) {
     return min_coins[money];
 }
 
-void PrintCoins(const int coins_used[], int money) {
+void PrintCoins(const int coins_used[], int money) 
+{
     int coin = money;
-    while (coin > 0) {
+    while (coin > 0) 
+    {
         int this_coin = coins_used[coin];
         printf("%d ", this_coin);
         coin = coin - this_coin;
